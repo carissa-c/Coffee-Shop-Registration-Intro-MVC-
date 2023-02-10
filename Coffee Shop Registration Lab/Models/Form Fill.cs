4dd45b2dd640d1eae67a8e65bddfb4ac2aa0c8cf
carissa-c <122126939+carissa-c@users.noreply.github.com>
2023-02-09 18:36:48 -0600

    first upload

---
+++ Coffee Shop Registration Lab/Models/Form Fill.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Coffee_Shop_Registration_Lab.Models
+{
+	public class Form_Fill
+	{
+		public string _firstName { get; set; }
+        public string _lastName { get; set; }
+    }
+}
+
